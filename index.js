75d214398a7eb533911783eacb7ac3b0fc629bff
Rojae Richards <rojae.richards@rfidientikit.com>
2023-03-25 14:08:12 +0000

    add comments to index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+var app = express();
+
+app.set('view engine', 'ejs')
+port = process.env.port || '9001';
+
+//set the app to listen to the port and log out the url
+app.listen(port,() =>{
+
+    console.log(`listening to http://localhost:${port}`)
+})
+
+//get the path name and render in the index page
+app.get("/", (req,res)=>{
+    res.render("index")
+});
+
+app.use(express.static('views'));
+
