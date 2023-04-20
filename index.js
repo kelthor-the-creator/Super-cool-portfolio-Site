6c9061cba78d47d31ebb7cb5d33c44b70eb86696
Rojae Richards <rojae.richards@rfidientikit.com>
2023-04-20 10:50:59 +0100

    Update index text

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 var app = express();
+const path = require("path");
 
 app.set('view engine', 'ejs')
 port = process.env.port || '9001';
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'dist')))
+
+
 
 //set the app to listen to the port and log out the url
 app.listen(port,() =>{
@@ -16,4 +22,3 @@ app.get("/", (req,res)=>{
 });
 
 app.use(express.static('views'));
-
